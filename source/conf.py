2ba29e31ffb7cf57fec4143162fc07a94c296e74
Klas Marcks <klas.marcks@gmail.com>
2020-06-14 20:41:04 +0200

    Try remove author.

--- source/conf.py
+++ source/conf.py
@@ -140,7 +140,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'RadMap.tex', 'RadMap Documentation',
-     'RadMap', 'howto'),
+     '', 'howto'),
 ]
 
 
